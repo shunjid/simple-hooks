4debd9487c64771b220b76ce90639e8fbb348856
shunjid <shunjid.rahman@gmail.com>
2021-09-18 19:49:23 +0600

    Simplify state card

--- src/components/State/index.tsx
+++ src/components/State/index.tsx
@@ -4,26 +4,56 @@ import {CardTheme} from './types';
 import {themes, defaultTheme} from './data';
 
 const StateComponent = (): JSX.Element => {
-    const [theme, setTheme] = useState<CardTheme>(defaultTheme);
+    const [cardTitle, cardDescription, cardSelectLabel]: [
+        cardTitle: string,
+        cardDescription: string,
+        cardSelectLabel: string
+    ] = [
+        'Sample Card',
+        'When you select a color, then the theme of this card changes',
+        'Select a color'
+    ];
+    const [theme, setTheme]: [
+        theme: CardTheme,
+        setTheme: React.Dispatch<React.SetStateAction<CardTheme>>
+    ] = useState<CardTheme>(defaultTheme);
 
     const cardStyle: Record<string, string> = {
-        cardRoot: (() => `card text-${theme.foreground} bg-${theme.background || defaultTheme.foreground} mb-3`)(),
+        cardRoot: (() =>
+            `card text-${theme.foreground} bg-${
+                theme.background || defaultTheme.foreground
+            } mb-3`)(),
     };
+
+    const getSelectedTheme = (selectedThemeBackground: string): CardTheme => {
+        return themes.filter(
+            (eachTheme: CardTheme) => eachTheme.background === selectedThemeBackground
+        )[0];
+    }
 
     return (
         <div className={cardStyle.cardRoot} style={{maxWidth: '18rem'}}>
             <div className='card-body'>
-                <h5 className='card-title'>Sample Card</h5>
-                <p className='card-text'>When you select a color, then the theme of this card changes</p>
-
-                <label>Select a color</label>
+                <h5 className='card-title'>{cardTitle}</h5>
+                <p className='card-text'>{cardDescription}</p>
+                <label>{cardSelectLabel}</label>
                 <select
                     className='custom-select mx-1'
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setTheme(themes.filter((eachTheme: CardTheme) => eachTheme.background === e.target.value)[0])}
-                    onBlur={(e: React.ChangeEvent<HTMLSelectElement>) => setTheme(themes.filter((eachTheme: CardTheme) => eachTheme.background === e.target.value)[0])}
                     defaultValue={defaultTheme.background}
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                        setTheme(getSelectedTheme(e.target.value))
+                    }
+                    onBlur={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                        setTheme(getSelectedTheme(e.target.value))
+                    }
                 >
-                    {themes.map((eachTheme: CardTheme) => <option value={eachTheme.background} key={eachTheme.background}>{eachTheme.name}</option>)}
+                    {themes.map((eachTheme: CardTheme) => {
+                        return (
+                            <option value={eachTheme.background} key={eachTheme.background}>
+                                {eachTheme.name}
+                            </option>
+                        );
+                    })}
                 </select>
             </div>
         </div>
